5f9bc83bc868351268fcd786bcdecbe7e08a5728
TivonS <91038241+TivonS@users.noreply.github.com>
2021-10-03 19:12:34 -0600

    Update firstLabC++.cpp
    This is with little bit of code outside of class square roots and using % to find the exact remainder.

--- firstLabC++.cpp
+++ firstLabC++.cpp
@@ -7,7 +7,7 @@ to another.
 I hereby certify that this program represents my work and that the design
 and logic was completed without outside assistance.
 
-Signature: 
+Signature:
 */
 #include <iostream>
 #include <iomanip>
@@ -17,7 +17,7 @@ int main()
 {
     //creating different variables for the conversions
     double pWeight;
-    double height;
+    int height;
     double miles;
     double gallon;
     double year;
@@ -27,13 +27,13 @@ int main()
     //asking the questions and than recieving a input value
     std::cout << "How much does your package weigh in pounds? ";
     std::cin >> pWeight;
-    
+
     std::cout << "\nhow tall are you in inches? ";
-    std::cin >> height; 
+    std::cin >> height;
 
     std::cout << "\nhow many miles did you travel on your last trip? ";
     std::cin >> miles;
-    std::cout<<"\nhow many gallons did you use to travel on your last trip? ";
+    std::cout << "\nhow many gallons did you use to travel on your last trip? ";
     std::cin >> gallon;
 
     std::cout << "\nhow old are you in years? ";
@@ -42,29 +42,28 @@ int main()
     std::cout << "\nhow many troy pounds do you have? ";
     std::cin >> pound;
 
-    system("cls"); 
+    system("cls");
 
     // converting the inputted value to the different type of units
-    std::cout << "package weight: " << pWeight*16 << " ounces \n" << std::endl;
+    std::cout << "package weight: " << pWeight * 16 << " ounces \n" << std::endl;
 
-    std::cout << "you are " << height / 12 << " feet\n" << std::endl;
+    std::cout << "you are " << height / 12 << " feet and " << (height % 12)
+        << " inches \n" << std::endl;
 
     std::cout << "you averaged " << miles / gallon << " mpg\n" << std::endl;
 
-    std::cout << "your " << year << " year old or "; 
-    std::cout << year * 365 << " days old\n" << std::endl;
+    std::cout << "your " << year << " year old or "
+        << year * 365 << " days old\n" << std::endl;
 
-    std::cout<<"spamMail = "<< spamMail<< ", double spamMail = "<< spamMail*2;
+    std::cout << "spamMail = " << spamMail << ", double spamMail = " << spamMail * 2;
     //since I we havent talked about finding square roots I found it 
-    std::cout << ", square root of spamMail = 7.1416\n " << std::endl;
+    std::cout << ", square root of spamMail= " << sqrt(spamMail) << std::endl;
 
     //std::fixed is making sure the final number is not rounded
-    std::cout << std::fixed ;
+    std::cout << std::fixed;
     std::cout << "you have " << pound * 12 * 31.1035;
     std::cout << " ounces \n" << std::endl;
 
     return 0;
 
 }
-
-
