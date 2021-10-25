eef4600d501b125333586e7f4bfc0215e079e0bb
TivonS <91038241+TivonS@users.noreply.github.com>
2021-10-25 10:21:56 -0500

    Add files via upload
    Added variables and asked user questions

---
+++ labMinMax/labMinMax.cpp
@@ -0,0 +1,52 @@
+/*
+Tivon Straub
+The purpose of this lab is for the user to be able to input 8 integers and for
+the computer to be able to identify the biggest number and smallest number.
+
+I hereby certify that this program represents my work and that the design
+and logic was completed without outside assistance.
+
+Signature:
+*/
+#include <iostream>
+
+
+int main()
+{
+    //creating a variable to represent each number
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+
+    /*
+    asking user for the 8 numbers and storing the data in variables.
+    */
+    std::cout << 
+        "I can tell you the greatest/smallest number of a set of 8 numbers!";
+    std::cout << "\nNow give me your first number: ";
+    std::cin >> a;
+    std::cout << "\nYour second number: ";
+    std::cin >> b;
+    std::cout << "\nYour third number: ";
+    std::cin >> c;
+    std::cout << "\nYour fourth number: ";
+    std::cin >> d;
+    std::cout << "\nYour fifth number: ";
+    std::cin >> e;
+    std::cout << "\nYour sixth number: ";
+    std::cin >> f;
+    std::cout << "\nYour seventh number: ";
+    std::cin >> g;
+    std::cout << "\nYour eighth number: ";
+    std::cin >> h;
+
+       //disciminating data to find the largest number
+    
+       
+}
+
