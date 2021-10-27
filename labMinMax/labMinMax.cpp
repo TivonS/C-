8647baeab5b707c9cfcd8908f9df2fa5cb87d481
TivonS <91038241+TivonS@users.noreply.github.com>
2021-10-26 22:45:35 -0600

    Update labMinMax.cpp
    Don't ass ints when setting variables!!!

--- labMinMax/labMinMax.cpp
+++ labMinMax/labMinMax.cpp
@@ -2,10 +2,8 @@
 Tivon Straub
 The purpose of this lab is for the user to be able to input 8 integers and for
 the computer to be able to identify the biggest number and smallest number.
-
 I hereby certify that this program represents my work and that the design
 and logic was completed without outside assistance.
-
 Signature:
 */
 #include <iostream>
@@ -22,11 +20,13 @@ int main()
     int f;
     int g;
     int h;
+    int great;  // both great and least are used lower
+    int less;
 
     /*
     asking user for the 8 numbers and storing the data in variables.
     */
-    std::cout << 
+    std::cout <<
         "I can tell you the greatest/smallest number of a set of 8 numbers!";
     std::cout << "\nNow give me your first number: ";
     std::cin >> a;
@@ -45,8 +45,74 @@ int main()
     std::cout << "\nYour eighth number: ";
     std::cin >> h;
 
-       //disciminating data to find the largest number
-    
-       
-}
+    system("cls");
 
+    /*
+    disciminating data to find the largest number I did this by setting a
+    variable great and using if statements to compare the different numbers.
+    */
+    great = a;
+    if (b > great)
+    {
+        great = b;      
+    }
+   if (c > great)
+    {
+        great = c;    
+    }
+    if (d > great)
+    {
+        great = d;     
+    }
+     if (e > great)
+    {
+        great = e;       
+    }
+    if (f > great)
+    {
+        great = f;     
+    }
+    if (g > great)
+    {
+        great = g;       
+    }
+     if (h > great)
+    {
+        great = h;       
+    }
+     std::cout << great << " is the greatest number!" << std::endl;
+
+    /*
+    same procces that was used for greater is now used for less than
+    */
+     less = a;
+     if (b < less)
+     {
+         less = b;
+     }
+     if (c < less)
+     {
+         less = c;
+     }
+     if (d < less)
+     {
+         less = d;
+     }
+     if (e < less)
+     {
+         less = e;
+     }
+     if (f < less)
+     {
+         less = f;
+     }
+     if (g < less)
+     {
+         less = g;
+     }
+     if (h < less)
+     {
+         less = h;
+     }
+     std::cout << less << " is the smallest number!";
+}
